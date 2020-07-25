7b87f253b48ed49bf65d607a20a3a20c6f977570
Guilherme Bublitz <bublitz.stein@gmail.com>
2020-07-25 16:06:20 -0300

    Configs typeorm and create users model and migration

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import express from 'express';
 
+import './database';
+
 const app = express();
+
+app.use(express.json());
+// app.use(routes);
 
 app.get('/', (request, response) => {
   return response.json({ message: 'Ola enfermeira' });
