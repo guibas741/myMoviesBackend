5ed64dbd2df596c16f03f6bf1460227d8c76ffb5
Guilherme Bublitz <bublitz.stein@gmail.com>
2020-07-26 15:26:58 -0300

    Creates route to remove movies from watchlist and to add as favorite

--- src/routes/movies.routes.ts
+++ src/routes/movies.routes.ts
@@ -1,12 +1,13 @@
+import { getRepository } from 'typeorm';
 /* eslint-disable camelcase */
 import { Router } from 'express';
 import WatchMovieService from '../services/WatchMovieService';
+import Movie from '../models/Movie';
 
 const moviesRouter = Router();
 
 moviesRouter.post('/', async (request, response) => {
   try {
-    console.log(request.body);
     const { movie, user_id } = request.body;
 
     const watchMovieService = new WatchMovieService();
@@ -16,7 +17,66 @@ moviesRouter.post('/', async (request, response) => {
       movie,
     });
 
-    return response.json({ watchedMovie });
+    return response.json(watchedMovie);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+moviesRouter.get('/', async (request, response) => {
+  try {
+    const moviesRepository = getRepository(Movie);
+    const { user_id } = request.body;
+
+    const movieList = await moviesRepository.find({ where: { user_id } });
+
+    return response.json(movieList);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+moviesRouter.put('/', async (request, response) => {
+  try {
+    const moviesRepository = getRepository(Movie);
+    const { user_id, movie_id } = request.body;
+
+    const movie = await moviesRepository.findOne({
+      where: { user_id, id: movie_id },
+    });
+
+    if (!movie) {
+      throw new Error('Movie not found');
+    }
+
+    movie.is_favorite = !movie.is_favorite;
+
+    await moviesRepository.save(movie);
+
+    return response.json(movie);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+moviesRouter.delete('/', async (request, response) => {
+  try {
+    const moviesRepository = getRepository(Movie);
+    const { user_id, movie_id } = request.body;
+
+    const movie = await moviesRepository.findOne({
+      where: { user_id, id: movie_id },
+    });
+
+    if (!movie) {
+      throw new Error('Movie not found');
+    }
+
+    await moviesRepository.remove(movie);
+
+    return response
+      .status(204)
+      .json({ success: 'Movie remove from watch list' });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
